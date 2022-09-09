74d6afebae78d45e9219cdf1107339bee0d895bd
Sagar Adhikari <adhikarisagar483@gmail.com>
2022-09-09 10:48:26 +0530

    initial commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../Context/AuthContext";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const { user, logIn } = UserAuth();
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await logIn(email, password);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+      setError(err.message);
+    }
+  };
+
+  return (
+    <>
+      <div className="w-full h-screen">
+        <img
+          className="hidden sm:block absolute w-full h-full object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/908077b4-cf0a-43c3-b2c9-435fb990299b/7d5fd507-5575-42f8-912d-14afd6cedc28/IN-en-20220829-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="/"
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
+        <div className="fixed w-full px-4 py-24 z-50">
+          <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+            <div className="max-w-[320px] mx-auto py-16">
+              <h1 className="text-3xl font-bold">Sign In</h1>
+              {error ? (
+                <p className="p-3 bg-red-400 my-2">
+                  Wrong Password or Username!!!
+                </p>
+              ) : null}
+              <form
+                onSubmit={handleSubmit}
+                className="w-full flex flex-col py-4"
+              >
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="p-3 my-2 bg-gray-700 rouded"
+                  type="email"
+                  placeholder="Email"
+                  autoComplete="email"
+                />
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="p-3 my-2 bg-gray-700 rouded"
+                  type="password"
+                  placeholder="Password"
+                  autoComplete="current-password"
+                />
+                <button className="bg-red-600 py-3 my-6 rounded font-bold">
+                  Sign In
+                </button>
+                <div className="flex justify-between items-center text-sm text-gray-600">
+                  <p>
+                    <input className="mr-2" type="checkbox" />
+                    Remember me
+                  </p>
+                  <p>Need Help?</p>
+                </div>
+                <p className="py-8">
+                  <span className="text-gray-600">No Netflix Account yet?</span>{" "}
+                  <Link to="/Signup">Sign Up</Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
